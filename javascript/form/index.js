14040f95bb85d45b49d2ce28d14b24d6b972eac0
irohanrajput <itusharsingh27@gmail.com>
2023-10-15 22:46:16 +0530

    js

---
+++ javascript/form/index.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('form').onsubmit = function(){
+        const name = document.querySelector('#name').value;
+        alert(`Hello ${name}!`)
+    }
+ })
