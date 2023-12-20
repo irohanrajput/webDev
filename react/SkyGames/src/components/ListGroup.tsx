b209eae9160d531162b4e8bf4eaf11353ab543aa
irohanrajput <itusharsingh27@gmail.com>
2023-12-20 06:39:18 +0530

    project structure, components, fragments.etc

---
+++ react/SkyGames/src/components/ListGroup.tsx
@@ -0,0 +1,25 @@
+// import { Fragment } from "react";
+
+function ListGroup() {
+
+  return (
+    // <Fragment>
+    //<div> 
+
+      <>
+      <h1>Helloooooo</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+      </>
+      
+    //</div>
+    // </Fragment>
+  );
+}
+
+export default ListGroup;
