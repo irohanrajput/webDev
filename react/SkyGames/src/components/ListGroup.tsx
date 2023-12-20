d093c6d514e6160d40c1032db22a399d28c06823
irohanrajput <itusharsingh27@gmail.com>
2023-12-20 07:49:12 +0530

    basics of react

--- react/SkyGames/src/components/ListGroup.tsx
+++ react/SkyGames/src/components/ListGroup.tsx
@@ -1,24 +1,30 @@
-// import { Fragment } from "react";
-
+import { MouseEvent } from "react";
 function ListGroup() {
+  let items = ["New York", "Delhi", "Miami", "Dubai", "Paris", "California"];
+  let selectedIndex = -1;
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
-    // <Fragment>
-    //<div> 
-
-      <>
-      <h1>Helloooooo</h1>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            } 
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
-      </>
-      
-    //</div>
-    // </Fragment>
+    </>
   );
 }
 
