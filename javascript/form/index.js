fcb81aadca0cf33dcf15a99e13286d58d5431105
irohanrajput <itusharsingh27@gmail.com>
2025-03-21 02:31:27 +0530

    new branch for web3

--- javascript/form/index.js
+++
@@ -1,6 +0,0 @@
- document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('form').onsubmit = function(){
-        const name = document.querySelector('#name').value;
-        alert(`Hello ${name}!`)
-    }
- })
